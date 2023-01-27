1650b9f982eab1e2e6e2c147eafbe37ee057ca8c
Drew Neece <dneece5@aggienetwork.com>
2023-01-27 12:19:11 -0600

    add StarshipList.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css'
-import NavBar from './components/NavBar/NavBar';
+import NavBar from './components/NavBar/NavBar'
+import StarshipList from './pages/StarshipList/StarshipList';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Routes>
+        <Route path="/starship-list" element={<StarshipList />} />
+      </Routes>
     </div>
   )
 }
 
-export default App;
+export default App
---
+++ src/pages/StarshipList/StarshipList.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+const StarshipList = () => {
+  const [starshipList, setStarshipList] = useState([])
+
+  return (
+    < >
+      <h3>Starship List</h3>
+    </>
+  )
+}
+
+export default StarshipList
